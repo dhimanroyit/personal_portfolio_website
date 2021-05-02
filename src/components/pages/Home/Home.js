385ba4bf22ac26756ee1abc76ad8889d0849dbd6
dhimanroyit <dhimanroyict@gmail.com>
2021-05-02 18:18:11 +0600

    update home image to video hero

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import hero from '../../../assets/img/hero.jpg';
+import hero from '../../../assets/video/hero.mp4';
 import Avatar from '../../Avatar/Avatar';
 import profilePic from '../../../assets/img/profile_pic.jpg'
 import Button from '../../Button/Button';
@@ -7,13 +7,13 @@ import Button from '../../Button/Button';
 function Home() {
   return (
   <div className="h-screen relative flex justify-center items-center">
-    <img className="absolute inset-0 block w-full h-full z-10" src={hero} alt=""/>
+    <video autoPlay muted loop className="absolute inset-0 block w-full h-full z-10 object-cover" src={hero} ></video>
     <div className="absolute inset-0 bg-gray-900 z-20 opacity-70"></div>
     <div className="z-30 flex flex-col items-center">
       <Avatar img={profilePic} className="w-52 h-52" />
-      <h1 className="text-7xl mt-6 text-white">Dhiman Roy</h1>
-      <p className="text-white mt-4">Front-End Developer</p>
-      <Button className="mt-10">Download Resume</Button>
+      <h1 className="text-7xl mt-6 uppercase text-white">Dhiman Roy</h1>
+      <p className="text-white mt-4 uppercase">Front-End Developer</p>
+      <Button className="mt-10 uppercase">Download Resume</Button>
     </div>
   </div>
   )
