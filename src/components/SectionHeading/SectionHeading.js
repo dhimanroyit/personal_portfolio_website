8f3212011e8b2454b8322cbc10f165c60aef7701
dhimanroyit <dhimanroyict@gmail.com>
2021-05-04 14:03:48 +0600

    section heading shape

--- src/components/SectionHeading/SectionHeading.js
+++ src/components/SectionHeading/SectionHeading.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SectionHeading({children}) {
   return (
-    <header className="py-2.5 p-5 mt-3 bg-theme-primary w-max relative section-heading">
+    <header className="py-2.5 p-5 bg-theme-primary w-max relative section-heading">
       <h1 className="text-2xl text-white uppercase font-bold">{children}</h1>
     </header>
   )
